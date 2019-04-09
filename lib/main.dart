87745855ce608cf9d6971e97b3ccd1af95b43c79
Jarkko <jarkkohei@luukku.com>
2019-04-09 21:41:21 +0300

    Extract the App-widget into its own file under src-folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Pics'),
-      ),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          elevation: 10.0,
-          onPressed: () {}),
-    ),
-  );
-
-  runApp(app);
+  runApp(App());
 }
---
+++ lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Pics'),
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add), elevation: 10.0, onPressed: () {}),
+      ),
+    );
+  }
+}
