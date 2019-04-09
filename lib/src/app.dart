fa8eb45924109016431baa4bb983b6ad03035d08
Jarkko <jarkkohei@luukku.com>
2019-04-09 22:25:07 +0300

    Refactor the App-widget to StatefulWidget and add _counter-state

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int _counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -8,8 +15,15 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: Text('Pics'),
         ),
+        body: Text('$_counter'),
         floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add), elevation: 10.0, onPressed: () {}),
+            child: Icon(Icons.add), 
+            elevation: 10.0, 
+            onPressed: () {
+              setState(() {
+                _counter += 1;
+              });
+            }),
       ),
     );
   }
